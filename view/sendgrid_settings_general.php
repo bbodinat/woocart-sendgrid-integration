b42aa1e89c02e581b931920f30c2458ecb7f58bf
Janez Troha <dz0ny@ubuntu.si>
2020-04-14 15:46:32 +0200

    Simplify settings

--- view/sendgrid_settings_general.php
+++ view/sendgrid_settings_general.php
@@ -81,24 +81,6 @@
           </td>
         </tr>
         <tr valign="top">
-          <th scope="row"><?php _e("Categories: "); ?></th>
-          <td>
-            <input type="text" name="sendgrid_categories" value="<?php echo $categories; ?>" size="20" class="regular-text" <?php disabled( defined('SENDGRID_CATEGORIES') ); ?>>
-            <span><small><em><?php _e('Leave blank to send without categories.') ?></em></small></span>
-            <p class="description"><?php _e('Associates the category of the email this should be logged as. <br />
-            Categories must be separated by commas (Example: category1,category2).') ?></p>
-          </td>
-        </tr>
-        <tr valign="top">
-          <th scope="row"><?php _e("Template: "); ?></th>
-          <td>
-            <input type="text" name="sendgrid_template" value="<?php echo $template; ?>" size="20" class="regular-text" <?php disabled( defined('SENDGRID_TEMPLATE') ); ?>>
-            <span><small><em><?php _e('Leave blank to send without template.') ?></em></small></span>
-            <p class="description"><?php _e('The template ID used to send emails. <br />
-            Example: 0b1240a5-188d-4ea7-93c1-19a7a89466b2.') ?></p>
-          </td>
-        </tr>
-        <tr valign="top">
           <th scope="row"><?php _e("Content-type: "); ?></th>
           <td>
             <select name="content_type" class="sendgrid-settings-select" id="content_type" <?php disabled( $is_env_content_type ); ?> >
@@ -107,54 +89,9 @@
             </select>
           </td>
         </tr>
-        <tr valign="top">
-          <th scope="row"><?php _e("Unsubscribe Group: "); ?></th>
-          <td>
-            <select id="select_unsubscribe_group" class="sendgrid-settings-select" name="unsubscribe_group" <?php disabled( $is_env_unsubscribe_group ); ?> <?php disabled( $no_permission_on_unsubscribe_groups ); ?>>
-              <option value="0"><?php _e("Global Unsubscribe"); ?></option>
-              <?php
-                if ( false != $unsubscribe_groups ) {
-                  foreach ( $unsubscribe_groups as $key => $group ) {
-                    if ( $unsubscribe_group_id == $group['id'] ) {
-                      echo '<option value="' . $group['id'] . '" selected="selected">' . $group['name'] . '</option>';
-                    } else {
-                      echo '<option value="' . $group['id'] . '">' . $group['name'] . '</option>';
-                    }
-                  }
-                }
-              ?>
-            </select>
-            <p class="description"><?php _e("User will have the option to unsubscribe from the selected group. <br /> The API Key needs to have 'Unsubscribe Groups' permissions to be able to select a group.") ?></p>
-          </td>
-        </tr>
       </tbody>
     </table>
   <br />
-  <table class="form-table">
-    <tbody>
-      <tr valign="top">
-            <td colspan="2">
-              <h3><?php echo _e('Statistics settings') ?></h3>
-            </td>
-        </tr>
-      <tr valign="top">
-        <th scope="row"><?php _e("Categories: "); ?></th>
-        <td>
-          <input type="text" name="sendgrid_stats_categories" value="<?php echo $stats_categories; ?>" size="20" class="regular-text" <?php disabled( defined('SENDGRID_STATS_CATEGORIES') ); ?>>
-          <span><small><em><?php _e('Leave blank for not showing category stats.') ?></em></small></span>
-          <p class="description"><?php _e('Add some categories for which you would like to see your stats. <br />
-          Categories must be separated by commas (Example: category1,category2).') ?></p>
-        </td>
-      </tr>
-      <tr valign="top">
-        <td colspan="2">
-          <p>
-            <?php _e('Disabled fields in this form means that they are already configured in the config file.'); ?>
-          </p>
-        </td>
-      </tr>
-    </tbody>
-  </table>
   <p class="submit">
     <input class="button button-primary" type="submit" name="Submit" value="<?php _e('Update Settings') ?>" />
   </p>
